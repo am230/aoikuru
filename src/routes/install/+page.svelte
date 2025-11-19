a94b0471065ac206d8b6809d4ba1376c569609d2
2.30am <111672334+am230@users.noreply.github.com>
2025-11-19 09:35:35 +0900

    Update +page.svelte

--- src/routes/install/+page.svelte
+++ src/routes/install/+page.svelte
@@ -1,67 +1,19 @@
 <script lang="ts">
     import { Button } from "@omujs/ui";
-    import { onMount } from "svelte";
-    import { getUrl } from "../constants";
-
-    let installState:
-        | {
-              type: "loading";
-          }
-        | {
-              type: "installing";
-              hostname: string;
-          }
-        | {
-              type: "installed";
-          }
-        | {
-              type: "failed";
-              message: string;
-          } = $state({ type: "loading" });
-
-    async function install() {
-        installState = { type: "installing", hostname: location.hostname };
-        try {
-            const resp = await fetch("http://localhost:26423/index_install", {
-                method: "POST",
-                body: JSON.stringify({
-                    index: getUrl(`/aoikuru/apps.json`),
-                    id: "io.github.am230:aoikuru",
-                }),
-            });
-            if (!resp.ok) {
-                installState = { type: "failed", message: resp.statusText };
-            } else {
-                installState = { type: "installed" };
-            }
-        } catch (e) {
-            installState = {
-                type: "failed",
-                message: "起動していない可能性があります",
-            };
-        }
-    }
-
-    onMount(install);
 </script>
 
 <main>
-    <div class="card">
-        {#if installState.type === "installing"}
-            <h2>管理画面から提供元を追加してください</h2>
-            <small>
-                {installState.hostname}
-                からの提供であることを確認してください
-            </small>
-        {:else if installState.type === "failed"}
-            <h2>追加に失敗しました</h2>
-            <small>メッセージ: {installState.message}</small>
-            <Button primary onclick={install}>もう一度試す</Button>
-        {:else if installState.type === "installed"}
-            <h2>追加が完了しました！</h2>
-            <small>このタブを閉じる事ができます</small>
-        {/if}
-    </div>
+    <p>アプリをインストールするには以下のリンクから提供元を追加してください</p>
+    <Button
+        onclick={() => {
+            window.open(
+                "http://localhost:26423/index/install?index_url=https://am230.github.io/aoikuru/apps.json",
+            );
+        }}
+        primary
+    >
+        提供元を追加
+</Button>
 </main>
 
 <style lang="scss">
@@ -72,29 +24,12 @@
         flex-direction: column;
         align-items: center;
         justify-content: center;
-        font-size: 2rem;
         color: var(--color-1);
         font-weight: 700;
         background: var(--color-bg-1);
     }
 
-    h2 {
-        font-size: 1.5rem;
-        border-bottom: 2px solid var(--color-1);
-    }
-
-    .card {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        gap: 0.5rem;
-        background: var(--color-bg-2);
-        padding: 4rem 6rem;
-    }
-
-    small {
-        font-size: 1rem;
-        color: var(--color-text);
+    p {
+        margin-bottom: 1rem;
     }
 </style>
